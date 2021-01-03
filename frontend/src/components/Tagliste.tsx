a2496b3c2de20261e1dd9a3f4b085f6c8ea0ffb8
Simon Zengerling <simon.zengerling@lht.dlh.de>
2021-01-03 02:11:46 +0100

    refactor(tagliste): string => tag type
    - tagliste now uses a Tag[]
    - divider added to tagliste

--- frontend/src/components/Tagliste.tsx
+++ frontend/src/components/Tagliste.tsx
@@ -1,18 +1,20 @@
 import React from 'react';
-import { Tag } from 'antd';
+import { Divider, Tag as AntdTag } from 'antd';
+import Tag from 'types/tag';
 
 const tagColors = ['magenta', 'red', 'green', 'volcano', 'orange', 'gold', 'lime']
 
 interface TaglisteProps {
-    tags: string[]
+    tags: Tag[]
 }
 
 function Tagliste(props: TaglisteProps){
     return (
         <div>
-            {props.tags && props.tags.map(tag => <Tag key={tag} color={tagColors[Math.floor(Math.random() * tagColors.length)]}>
-                {tag}
-            </Tag>)}
+            <Divider />
+            {props.tags && props.tags.map(tag => <AntdTag key={+tag.id} color={tagColors[Math.floor(Math.random() * tagColors.length)]}>
+                {tag.title}
+            </AntdTag>)}
         </div>
     )
 }
